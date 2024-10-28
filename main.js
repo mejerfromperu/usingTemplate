3c3d0ddd952c4b86dc4003fa356de884e2fc3b6d
mejerfromperu <144741518+mejerfromperu@users.noreply.github.com>
2024-10-28 14:31:12 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            intro: 'Welcome to my Vue template',
+        }
+    },
+    methods: { 
+        myMethod() {
+            
+        },
+    },
+    computed: {
+        myComputed() { 
+            return ''; 
+        }
+    }
+});
