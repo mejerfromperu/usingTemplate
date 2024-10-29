3f7373d7f92fce0e3c37d9393a08b72481389194
mejerfromperu <christianmejer7@gmail.com>
2024-10-29 14:23:33 +0100

    opg 29-10-2024

--- main.js
+++ main.js
@@ -1,13 +1,59 @@
 const app = Vue.createApp({
     data() {
         return {
-            intro: 'Welcome to my Vue template',
-        }
+            stocks: [
+                {
+                    name: "RXP",
+                    purchasePrice: 3,
+                    salePrice: 10,
+                    quantity: 100,
+                    sum: function() {
+                        return this.purchasePrice * this.quantity;
+                    }
+                },
+                {
+                    name: "ETH",
+                    purchasePrice: 10,
+                    salePrice: 110,
+                    quantity: 43,
+                    sum: function() {
+                        return this.salePrice * this.quantity;
+                    }
+                }
+            ],
+            newName: "",
+            newPurchasePrice: null,
+            newSalePrice: null,
+            newQuantity: 1,
+            portfolioValue: 0,
+        };
     },
     methods: { 
-        myMethod() {
-            
+        addStock() {
+            const newStock = {
+                name: this.newName,
+                purchasePrice: this.newPurchasePrice,
+                salePrice: this.newSalePrice,
+                quantity: this.newQuantity,
+                sum() {
+                    return (this.purchasePrice || this.salePrice) * this.quantity;
+                }
+            };
+            this.stocks.push(newStock);
+            // Nulstil inputfelterne
+            this.newName = "";
+            this.newPurchasePrice = null;
+            this.newSalePrice = null;
+            this.newQuantity = 1;
         },
+        
+
+        
+        totalPortofoile() {
+            this.portfolioValue = this.stocks.reduce((total, stock) => {
+                return total + stock.sum();
+            }, 0);
+        }
     },
     computed: {
         myComputed() { 
